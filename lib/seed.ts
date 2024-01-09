8bbf2975ebf13a703373bc7f80856ca3042e530a
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-09 22:37:33 +0200

    initial testing

---
+++ lib/seed.ts
@@ -0,0 +1,19 @@
+import { db, sql } from './kysely';
+
+export async function seed() {
+  const createUsersTable = await db.schema
+    .createTable('users')
+    .ifNotExists()
+    .addColumn('id', 'serial', (cb) => cb.primaryKey())
+    .addColumn('name', 'varchar(255)', (cb) => cb.notNull())
+    .addColumn('email', 'varchar(255)', (cb) => cb.notNull().unique())
+    .addColumn('image', 'varchar(255)')
+    .addColumn('createdAt', sql`timestamp with time zone`, (cb) =>
+      cb.defaultTo(sql`current_timestamp`),
+    )
+    .execute();
+  console.log(`Created "users" table`);
+  return {
+    createUsersTable,
+  };
+}
