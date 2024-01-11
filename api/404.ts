f9592aad295a6edd1bc89b3b3ca0952bcdc05275
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-11 02:10:59 +0200

    404 code

--- api/404.ts
+++ api/404.ts
@@ -1,8 +1,9 @@
 import { type VercelRequest, type VercelResponse } from '@vercel/node';
+import { ApiError } from '../models';
 
 export default async function handler(
   _: VercelRequest,
   res: VercelResponse,
 ): Promise<VercelResponse> {
-  return res.status(404).json({ message: 'Not found' });
+  return res.status(404).json({ code: ApiError.NOT_FOUND });
 }
